a13fa5b9e521c5210727e0544c931b0410af3939
Kris Leland <kristopherleland@gmail.com>
2022-02-09 19:32:14 -0500

    working on the first bloc for the map location stuff

---
+++ lib/blocs/map_bloc/map_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'map_state.dart';
+
+class MapCubit extends Cubit<MapState> {
+  MapCubit(MapState initialState) : super(initialState);
+
+}
---
+++ lib/blocs/map_bloc/map_state.dart
@@ -0,0 +1,8 @@
+part of './map_bloc.dart';
+
+class MapState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+
+}
